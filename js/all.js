bed19df5ff95c09703b982f45455d7dda2759908
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-07-03 02:38:52 +0800

    [add] access data api to render view

--- js/all.js
+++ js/all.js
@@ -2,6 +2,7 @@ import card from "./components/card.js";
 import banner from "./components/banner.js";
 import pagination from "./components/pagination.js";
 import footer from "./components/footer.js";
+import { apiProductList } from "./components/api.js";
 
 
 let banner_section = document.querySelector("#banner_section");
@@ -9,22 +10,52 @@ let product_section = document.querySelector("#product_section");
 let pagenum_section = document.querySelector("#pagenum_section");
 let footer_section =  document.querySelector("#footer_section");
 
-footer_section.innerHTML = footer.render();
 
-pagenum_section.innerHTML = pagination.render();
 
-banner_section.innerHTML = banner.render();
+apiProductList()
+    .then( res => {
+        // console.log(res);
+        render(res.data, 0);
+    })
+    .catch( err => {
+        console.log(err);        
+    });
 
-let data_obj = {
-    viewer_num: 10,
-    title: "京都一日遊",
-    category: "京都",
-    origin_price: 2000,
-    price: 1800,
-    img_url_list: ["https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-    ],
+
+function render(data, start) {
+
+    let productList = data.data;
+
+    productList = productList.slice(start, start+3);
+
+    // banner view
+    let img_url_list = [];
+    productList.forEach( product => {
+        img_url_list.push(product.imageUrl[0]);
+    });
+
+    banner.load(img_url_list);
+    banner_section.innerHTML = banner.render();
+    $("#image_carousel").carousel();
+
+    // product view
+    let all_product_view = "";
+    productList.forEach( product => {
+        let data_obj = {
+            viewer_num: product.options.viewer_num,
+            title: product.title,
+            category: product.category,
+            origin_price: product.origin_price,
+            price: product.price,
+            img_url_list: product.imageUrl,
+        }
+        card.load(data_obj);
+        all_product_view += card.render();
+    });
+
+    product_section.innerHTML = all_product_view;
 }
 
-card.load(data_obj);
-product_section.innerHTML = card.render() + card.render() + card.render();
 
+pagenum_section.innerHTML = pagination.render();
+footer_section.innerHTML = footer.render();
