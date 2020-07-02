ffe076316290d31cd1a8efe06d42b80b404b5618
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-07-03 02:37:46 +0800

    [modify] auto-generate carousel-item

--- js/components/banner.js
+++ js/components/banner.js
@@ -1,5 +1,5 @@
 export default {
-    img_url_list: ["https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"],
+    img_url_list: [],
 
     load(img_url_list){
         this.img_url_list = img_url_list;
@@ -7,27 +7,35 @@ export default {
 
     render() {
 
-        let img_url = this.img_url_list[0];
+        let img_template = "";
+        let is_first = true;
+        this.img_url_list.forEach( img_url => {
+            
+            let active = "";
+            if(is_first){
+                active = "active";
+                is_first = false;
+            }
+            else{
+                active = "";
+            }
+
+            img_template += `
+            <div class="carousel-item ${active}">
+                <img class="d-block w-100 " src="${img_url}">
+            </div>
+            `;
+        });
 
         let template = `
         <div class="col">
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div id="image_carousel" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="${img_url}" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="${img_url}" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="${img_url}" alt="Third slide">
-                    </div>
+                    ${img_template}
                 </div>
             </div>
         </div>
       `;
-
-
 
         return template;
     }
